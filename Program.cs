131ebeb676f0a90f98a5af002d3057b1b262c6fe
Alejandro <alejandromatos1302@gmail.com>
2020-07-18 20:34:00 -0400

    V0.1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace PRACTICA_FINAL
+﻿using System;
+
+namespace PRACTICA_FINAL
 {
+    //ALEJANDRO MATOS 2019-8540
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("v0.1");
         }
     }
 }
