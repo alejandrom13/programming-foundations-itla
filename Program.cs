ec0d76c5039d09b93a57f9e7aaadfbac7e20af59
Alejandro <alejandromatos1302@gmail.com>
2020-07-19 16:24:26 -0400

    v0.6

--- Program.cs
+++ Program.cs
@@ -16,74 +16,89 @@ namespace PRACTICA_FINAL
     public static void Main(string[] args)
     {
       
-      
       Hashtable listaEmpleados = new Hashtable();
-
+      
+      //EMPLEADO 1
       Empleado empleado1 = new Empleado()
       {
         nombre = "ALEJANDRO ANTONIO MATOS CIPRIAN",
         creacion = new DateTime(2002, 2, 13),
         activa = true,
-        rol = "SUPERVISOR"
-
+        rol = "SUPERVISOR",
       };
-      //CREDENCIALES 1
-      listaEmpleados.Add("40213664937", empleado1);
-      listaEmpleados.Add("13022002", empleado1);
-      
+       listaEmpleados.Add("40213664937", empleado1);
+       listaEmpleados.Add(empleado1, "13022002");
+       
+      //EMPLEADO 2 
       Empleado empleado2 = new Empleado()
       {
         nombre = "MIGUEL RAMON MORETA JIMENEZ",
         creacion = new DateTime(2005, 5, 1),
         activa = true,
-        rol = "ADMINISTRADOR"
+        rol = "ADMINISTRADOR",
       };
-      //CREDENCIALES 2
       listaEmpleados.Add("01000042413", empleado2);
-      listaEmpleados.Add("01052005", empleado2);
+      listaEmpleados.Add(empleado2, "01052005");
       
-      
+      //EMPLEADO 3
       Empleado empleado3 = new Empleado()
       {
         nombre = "JAVIER DE LOS SANTOS RAMIREZ",
         creacion = new DateTime(2004, 7, 10),
         activa = false,
-        rol = "VENDEDOR"
-      };
-      //CREDENCIALES 3
-      listaEmpleados.Add("01000013215", empleado3);
-      listaEmpleados.Add("10072004", empleado3);
+        rol = "VENDEDOR",
 
-      Console.Write("Ingrese su numero de cédula: ");
+      };
+      listaEmpleados.Add("01000013215", empleado3);
+      listaEmpleados.Add(empleado3, "10072004");
+      
+      Console.Write("INGRESE SU NUMERO DE CEDULA: ");
       string cedula_empleado = Console.ReadLine();
       
-      Console.Write("Ingrese su clave: ");
-      string clave_empleado = Console.ReadLine();
-
-      while (!listaEmpleados.ContainsKey(cedula_empleado) && (!listaEmpleados.ContainsKey(clave_empleado)))
-      {
-        Console.WriteLine("-------");
-        Console.WriteLine("INCORRECTO");
-        Console.WriteLine("-------");
-        Console.Write("Ingrese su numero de cédula: ");
-        cedula_empleado = Console.ReadLine();
-        
-        Console.Write("Ingrese su clave: ");
-        clave_empleado = Console.ReadLine();
-      }
       
-      if (!listaEmpleados.ContainsKey(cedula_empleado) && (!listaEmpleados.ContainsKey(clave_empleado)))
-      {
-      Empleado empleado = (Empleado) listaEmpleados[cedula_empleado];
-      Console.WriteLine("Bienvenido a la plataforma Sr. " + empleado.nombre);
-      Console.WriteLine("Su cuenta está: {0} y fue creada el {1}", empleado.activa, empleado.creacion);
-      }
+      while (!listaEmpleados.ContainsKey(cedula_empleado))
+              {
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("CEDULA NO ENCONTRADA EN EL SISTEMA");
+                Console.WriteLine("----------------------------------");
+                Console.Write("INGRESE SU NUMERO DE CEDULA NUEVAMENTE: ");
+                cedula_empleado = Console.ReadLine();
+              }
+              
+                Console.Write("INGRESE SU CLAVE: ");
+                string clave_empleado = Console.ReadLine();
+                
+                
+                while (!listaEmpleados.ContainsValue(clave_empleado))
+                {
+                  Console.WriteLine("----------------------------------");
+                  Console.WriteLine("CLAVE NO ENCONTRADA EN EL SISTEMA");
+                  Console.WriteLine("----------------------------------");
+                  Console.Write("INGRESE SU CLAVE NUEVAMENTE: ");
+                  clave_empleado = Console.ReadLine();
+                }
+
+              
+      if (!listaEmpleados.ContainsKey(cedula_empleado) && listaEmpleados.ContainsValue(clave_empleado))
+       {
+         Console.WriteLine("DATOS EQUIVOCADOS");
+       }
       else
       {
-              Console.WriteLine("CEDULA EQUIVOCADA");
-              return;
-      }
+        Empleado empleado = (Empleado) listaEmpleados[cedula_empleado];
+        Console.WriteLine("Bienvenido a la plataforma {0}", empleado.nombre);
+        Console.WriteLine("Usted ha accedido con el usuario {0}, y su rol es: {1}", cedula_empleado, empleado.rol);
+        if (empleado.activa) { Console.WriteLine("Su cuenta está: ACTIVA y fue creada el {0}", empleado.creacion); }
+        else { Console.WriteLine("SU CUENTA ESTA INACTIVA, POR LO TANTO NO PUEDE ACCEDER AL SISTEMA"); }
 
+      }
+      
+
+
+
+      
+
+     
 
 
 
